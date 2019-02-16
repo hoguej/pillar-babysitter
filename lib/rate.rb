4143032c1f85c9657b377d52a370a23830d53cbd
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 00:32:32 -0500

    should only be able to set valid end_times

--- lib/rate.rb
+++ lib/rate.rb
@@ -2,16 +2,21 @@ class Rate
   attr_reader :start_time, :end_time
 
   def start_time=(start_time)
-    error_message = "Start time must be after 5 pm and no later than 3 am"
+    error_message = 'Start time must be after 5 pm and no later than 3 am'
 
     valid_start_times = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3]
     unless valid_start_times.include?(start_time) 
-      raise "Start time must be after 5 pm and no later than 3 am"
+      raise 'Start time must be after 5 pm and no later than 3 am'
     end
     @start_time = start_time
   end
 
   def end_time=(end_time)
+    valid_end_times = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4]
+    unless valid_end_times.include?(end_time)
+      raise 'End time must be after 6 pm and no later than 4 am'
+    end
+
     @end_time = end_time
   end
 end
--- spec/rate_spec.rb
+++ spec/rate_spec.rb
@@ -15,9 +15,10 @@ describe Rate do
   end
 
   it 'should give an error if an invalid start_time' do
-    invalid_start_times = [0, 4, 13].each do |invalid_start_time|
+    invalid_start_times = [0, 4, 13]
+    invalid_start_times.each do |invalid_start_time|
       expect {
-        @rate.start_time = 0
+        @rate.start_time = invalid_start_time
       }.to raise_error "Start time must be after 5 pm and no later than 3 am"
     end
   end
@@ -25,5 +26,14 @@ describe Rate do
   it 'should be able to set an end time' do
     @rate.end_time = 6
     expect(@rate.end_time).to be(6)
+  end
+
+  it 'should give an error if an invalid end_time' do
+    invalid_end_times = [5, 0, 13]
+    invalid_end_times.each do |invalid_end_time|
+      expect {
+        @rate.end_time = invalid_end_time
+      }.to raise_error "End time must be after 6 pm and no later than 4 am"
+    end
   end
 end
