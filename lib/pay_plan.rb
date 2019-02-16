404cf02418633dde01a1cab6be69236434612039
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 11:09:00 -0500

    be able to add a pay_schedule to a pay_plan

--- lib/pay_plan.rb
+++ lib/pay_plan.rb
@@ -1,2 +1,11 @@
 class PayPlan
+  attr_reader :pay_schedules
+
+  def initialize
+    @pay_schedules = []
+  end
+
+  def add_pay_schedule(pay_schedule)
+    @pay_schedules << pay_schedule
+  end
 end
--- spec/pay_plan_spec.rb
+++ spec/pay_plan_spec.rb
@@ -1,8 +1,17 @@
 require_relative 'spec_helper'
 
 describe PayPlan do
-  it 'should create new PayPlan' do
+  before(:each) do
     @pay_plan = PayPlan.new
+  end
+
+  it 'should create new PayPlan' do
     expect(@pay_plan).to be_a(PayPlan)
+  end
+
+  it 'should be able to add a pay_schedule' do
+    pay_schedule = PaySchedule.new
+    @pay_plan.add_pay_schedule(pay_schedule)
+    expect(@pay_plan.pay_schedules).to eq([pay_schedule])
   end
 end
