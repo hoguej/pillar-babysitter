a07c8120aabc94b56bf13a37a06eae55892036bf
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 17:39:16 -0500

    copy end time from pay_schedule to shift

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,4 +1,6 @@
 class Shift
+  attr_accessor :start_time, :end_time
+
   def start_time=(start_time)
     error_message = 'Start time must be after 5 pm and no later than 3 am'
 
@@ -7,5 +9,14 @@ class Shift
       raise 'Start time must be after 5 pm and no later than 3 am'
     end
     @start_time = start_time
+  end
+
+  def end_time=(end_time)
+    valid_end_times = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4]
+    unless valid_end_times.include?(end_time)
+      raise 'End time must be after 6 pm and no later than 4 am'
+    end
+
+    @end_time = end_time
   end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -6,4 +6,32 @@ describe Shift do
   it 'should create a shift object' do
     expect(@shift).to be_a(Shift)
   end
+
+  it 'should be able to set a start time' do
+    @shift.start_time = 5
+    expect(@shift.start_time).to be(5)
+  end
+
+  it 'should give an error if an invalid start_time' do
+    invalid_start_times = [0, 4, 13]
+    invalid_start_times.each do |invalid_start_time|
+      expect {
+        @shift.start_time = invalid_start_time
+      }.to raise_error "Start time must be after 5 pm and no later than 3 am"
+    end
+  end
+
+  it 'should be able to set an end time' do
+    @shift.end_time = 6
+    expect(@shift.end_time).to be(6)
+  end
+
+  it 'should give an error if an invalid end_time' do
+    invalid_end_times = [5, 0, 13]
+    invalid_end_times.each do |invalid_end_time|
+      expect {
+        @shift.end_time = invalid_end_time
+      }.to raise_error "End time must be after 6 pm and no later than 4 am"
+    end
+  end
 end
