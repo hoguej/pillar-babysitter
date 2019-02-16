5e33804a484506408eae1db37207a5ec9720b030
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 17:43:22 -0500

    Move start_time / end_time logic to a Shift object which is a property on pay_schedule

--- lib/pay_schedule.rb
+++ lib/pay_schedule.rb
@@ -1,29 +1,29 @@
 class PaySchedule
-  attr_reader :start_time, :end_time, :rate
+  attr_reader :rate, :shift
 
   def initialize(params = {})
-    self.start_time = params[:start_time] if params[:start_time]
-    self.end_time = params[:end_time] if params[:end_time]
+    @shift = Shift.new
+    @shift.start_time = params[:start_time] if params[:start_time]
+    @shift.end_time = params[:end_time] if params[:end_time]
     self.rate = params[:rate] if params[:rate]
   end
 
   def start_time=(start_time)
-    error_message = 'Start time must be after 5 pm and no later than 3 am'
-
-    valid_start_times = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3]
-    unless valid_start_times.include?(start_time) 
-      raise 'Start time must be after 5 pm and no later than 3 am'
-    end
+    shift.start_time = start_time
     @start_time = start_time
   end
 
-  def end_time=(end_time)
-    valid_end_times = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4]
-    unless valid_end_times.include?(end_time)
-      raise 'End time must be after 6 pm and no later than 4 am'
-    end
+  def start_time
+    shift.start_time
+  end
 
+  def end_time=(end_time)
+    shift.end_time = end_time
     @end_time = end_time
+  end
+
+  def end_time
+    shift.end_time
   end
 
   def rate=(rate)
--- spec/pay_schedule_spec.rb
+++ spec/pay_schedule_spec.rb
@@ -14,27 +14,9 @@ describe PaySchedule do
     expect(@pay_schedule.start_time).to be(5)
   end
 
-  it 'should give an error if an invalid start_time' do
-    invalid_start_times = [0, 4, 13]
-    invalid_start_times.each do |invalid_start_time|
-      expect {
-        @pay_schedule.start_time = invalid_start_time
-      }.to raise_error "Start time must be after 5 pm and no later than 3 am"
-    end
-  end
-
   it 'should be able to set an end time' do
     @pay_schedule.end_time = 6
     expect(@pay_schedule.end_time).to be(6)
-  end
-
-  it 'should give an error if an invalid end_time' do
-    invalid_end_times = [5, 0, 13]
-    invalid_end_times.each do |invalid_end_time|
-      expect {
-        @pay_schedule.end_time = invalid_end_time
-      }.to raise_error "End time must be after 6 pm and no later than 4 am"
-    end
   end
 
   it 'should be able to set a PaySchedule' do
