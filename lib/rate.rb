24a372c68176e20cdf42ea8120138991b71af7fc
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 00:11:17 -0500

    start time shouldn't be less than 1

--- lib/rate.rb
+++ lib/rate.rb
@@ -1,3 +1,10 @@
 class Rate
-  attr_accessor :start_time
+  attr_reader :start_time
+
+  def start_time=(start_time)
+    if(start_time < 1)
+      raise "Start time must be above 1"
+    end
+    @start_time = start_time
+  end
 end
--- spec/rate_spec.rb
+++ spec/rate_spec.rb
@@ -13,4 +13,11 @@ describe Rate do
     @rate.start_time = 5
     expect(@rate.start_time).to be(5)
   end
+
+  it 'should give an error if start_time is less than 1' do
+    # 5-12, 1-3
+    expect {
+      @rate.start_time = 0
+    }.to raise_error 'Start time must be above 1'
+  end
 end
