7af9cae431060c6cf0846b64f31ff5df2c49bb9f
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-16 16:43:32 -0500

    Be able to set properties in PaySchedule.new

--- lib/pay_schedule.rb
+++ lib/pay_schedule.rb
@@ -1,6 +1,12 @@
 class PaySchedule
   attr_reader :start_time, :end_time, :rate
 
+  def initialize(params = {})
+    self.start_time = params[:start_time] if params[:start_time]
+    self.end_time = params[:end_time] if params[:end_time]
+    self.rate = params[:rate] if params[:rate]
+  end
+
   def start_time=(start_time)
     error_message = 'Start time must be after 5 pm and no later than 3 am'
 
