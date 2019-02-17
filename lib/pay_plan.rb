5210b8bd2f54270f0371dcbb9023b778ce7649fb
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-17 16:05:32 -0500

    Check for overlaps on pay plan when adding a pay schedule

--- lib/pay_plan.rb
+++ lib/pay_plan.rb
@@ -6,12 +6,17 @@ class PayPlan
     @rates_by_hour = {}
   end
 
-  def add_pay_schedule(pay_schedule)
-    calc_end_time = pay_schedule.end_time
-    @pay_schedules << pay_schedule
+  def add_pay_schedule(new_pay_schedule)
+    @pay_schedules.each do |existing_pay_schedule|
+      raise "New pay schedule should not overlap existing pay schedules" if existing_pay_schedule.overlaps?(new_pay_schedule)
+    end
+    @pay_schedules << new_pay_schedule 
   end
 
   def rate_for_hour(hour)
-    return 100
+    @pay_schedules.each do |pay_schedule|
+      return pay_schedule.rate if pay_schedule.include?(hour)
+    end
+    return nil
   end
 end
--- spec/pay_plan_spec.rb
+++ spec/pay_plan_spec.rb
@@ -21,11 +21,22 @@ describe PayPlan do
     expect(@pay_plan.rate_for_hour(5)).to eq(100)
   end
 
-  #it 'should give back the rate, given an hour of the day with multiple schedules' do
-    #pay_schedule1 = PaySchedule.new(start_time: 5, end_time: 6, rate: 100)
-    #pay_schedule2 = PaySchedule.new(start_time: 6, end_time: 7, rate: 9)
-    #@pay_plan.add_pay_schedule(pay_schedule1)
-    #@pay_plan.add_pay_schedule(pay_schedule2)
-    #expect([@pay_plan.rate(5), @pay_plan.rate(6)]).to eq([100, 9])
-  #end
+  it 'should give back the rate, given an hour of the day with multiple schedules' do
+    pay_schedule1 = PaySchedule.new(start_time: 5, end_time: 6, rate: 100)
+    pay_schedule2 = PaySchedule.new(start_time: 6, end_time: 7, rate: 9)
+    @pay_plan.add_pay_schedule(pay_schedule1)
+    @pay_plan.add_pay_schedule(pay_schedule2)
+    rate5 = @pay_plan.rate_for_hour(5)
+    rate6 = @pay_plan.rate_for_hour(6)
+    expect([rate5, rate6]).to eq([100, 9])
+  end
+
+  it 'should not allow you to add overlapping schedules where the second schedule is within the first' do
+    pay_schedule1 = PaySchedule.new(start_time: 5, end_time: 8, rate: 100)
+    pay_schedule2 = PaySchedule.new(start_time: 6, end_time: 7, rate: 9)
+    @pay_plan.add_pay_schedule(pay_schedule1)
+    expect {
+      @pay_plan.add_pay_schedule(pay_schedule2)
+    }.to raise_error
+  end
 end
