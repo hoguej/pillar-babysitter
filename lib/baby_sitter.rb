b94f75163c2ae9191ab3b9aa12710827d8ea1210
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-18 00:28:01 -0500

    Be able to caclculate pay for a shift

--- lib/baby_sitter.rb
+++ lib/baby_sitter.rb
@@ -1,6 +1,12 @@
 class BabySitter
-  #def calculate_pay_for_shift(params)
-    #family = params[:family]
-    #shift = params[:shift]
-  #end
+  def calculate_pay_for_shift(params)
+    family = params[:family]
+    shift = params[:shift]
+
+    total = 0
+    shift.hours.map do |hour|
+      total += family.pay_plan.rate_for_hour(hour)
+    end
+    return total
+  end
 end
