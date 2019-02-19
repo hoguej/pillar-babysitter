97a72c4f27aebef7810c1323ba2bfbee19edba5e
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-18 23:25:24 -0500

    Get rspec to use local bundle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,4 @@ require_relative '../lib/pay_plan'
 require_relative '../lib/shift'
 require_relative '../lib/family'
 require_relative '../lib/baby_sitter'
+require_relative "../bundle/bundler/setup"
