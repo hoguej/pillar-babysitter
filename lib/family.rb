2e882213f3cd276eba5ce28b0f613ecc79898454
Jonathan Hogue <jon.hogue@gmail.com>
2019-02-20 00:53:22 -0500

    Create a pay plan when creating a new family

--- lib/family.rb
+++ lib/family.rb
@@ -2,7 +2,11 @@ class Family
   attr_accessor :pay_plan, :name
 
   def initialize(params = {})
-    self.pay_plan = params[:pay_plan] if params[:pay_plan]
+    if(params[:pay_plan])
+      self.pay_plan = params[:pay_plan]
+    else
+      self.pay_plan = PayPlan.new
+    end
     self.name = params[:name] if params[:name]
   end
 
--- spec/family_spec.rb
+++ spec/family_spec.rb
@@ -1,19 +1,20 @@
 require_relative 'spec_helper'
 
 describe Family do
-  it "should create a new family" do
+  before(:each) do
     @family = Family.new
+  end
+
+  it "should create a new family" do
     expect(@family).to be_a(Family)
   end
 
   it "should be able to add a pay_plan to family" do
-    @family = Family.new
     @family.pay_plan = PayPlan.new
     expect(@family.pay_plan).to be_a(PayPlan)
   end
 
   it "should be able to set a name on family" do
-    @family = Family.new
     @family.name = 'testname'
     expect(@family.name).to eq('testname')
   end
@@ -32,5 +33,9 @@ describe Family do
     }
     result_classes = Family.load(families).map{ |family| family.class}
     expect(result_classes).to eq([Family, Family, Family])
+  end
+
+  it "should create a pay_plan if none is passed when creating a new family" do
+    expect(@family.pay_plan).to be_a(PayPlan)
   end
 end
